cf1818ed2897d9502d2d8cf919a64a18199f7f8b
Adetola Onasanya <adetola.onasanya@gmail.com>
2019-03-20 11:59:42 +0100

    Setup Cookies

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const createServer = require("./createServer");
 const db = require("./db");
+const jwt = require("jsonwebtoken");
 
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO Use express middlware to populate current user
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
