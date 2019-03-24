ce2ec229f8ebe3de688f044248822e1830abe433
Adetola Onasanya <adetola.onasanya@gmail.com>
2019-03-24 22:13:23 +0100

    Update User Model

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,6 +3,9 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   projects: forwardTo("db"),
   project: forwardTo("db"),
+  projectsConnection: forwardTo("db"),
+  users: forwardTo("db"),
+  user: forwardTo("db"),
   categories: forwardTo("db"),
   category: forwardTo("db"),
   async me(parent, args, ctx, info) {
